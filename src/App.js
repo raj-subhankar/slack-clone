ae7e875434fec0ad7c0ca81fca6fec07edb481d6
raj-subhankar <subhankar.rj@gmail.com>
2020-12-12 21:31:19 +0530

    Implement Sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
       <Header />
+
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
