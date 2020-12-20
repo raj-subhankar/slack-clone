b7d54354efdd89e2bf83ab664b1e928cba00faa3
Subhankar Raj <subhankar.rj@gmail.com>
2020-12-20 22:15:56 +0530

    Use User state

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -14,10 +14,12 @@ import ExpandLessIcon from '@material-ui/icons/ExpandLess'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import AddIcon from '@material-ui/icons/Add'
 import db from './firebase'
+import { useStateValue } from './StateProvider'
 
 function Sidebar() {
 
     const [channels, setChannels] = useState([]);
+    const [{ user }] = useStateValue();
 
     useEffect(() => {
         db.collection('rooms').onSnapshot(snapshot => (
@@ -35,7 +37,7 @@ function Sidebar() {
                     <h2>Test header</h2>
                     <h3>
                         <FiberManualRecordIcon />
-                        Subhankar
+                        {user?.displayName}
                     </h3>
                 </div>
                 <CreateIcon />
