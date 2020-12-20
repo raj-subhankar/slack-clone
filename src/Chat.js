4d2c1c766942b38db2bca0d6f30a987db9afd73e
Subhankar Raj <subhankar.rj@gmail.com>
2020-12-20 22:45:26 +0530

    Implement send messages

--- src/Chat.js
+++ src/Chat.js
@@ -5,6 +5,7 @@ import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined"
 import { useParams } from "react-router-dom"
 import db from './firebase'
 import Message from './Message'
+import ChatInput from "./ChatInput"
 
 function Chat() {
     const { roomId } = useParams();
@@ -56,6 +57,8 @@ function Chat() {
 
                 ))}
             </div>
+
+            <ChatInput channelName={roomDetails?.name} channelId={roomId} />
         </div>
     )
 }
